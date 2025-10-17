20f83008798c2ffea1eaab086f4472f168a72f8f
Liu-Qiuping <2513424754@qq.com>
2025-10-17 15:17:26 +0800

    Add files via upload

---
+++ 23区块链02班_2023113499_刘秋萍/03.3.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	//定义一个外层循环的标签
+OutterLoop:
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			fmt.Printf("正在检查: i:%d,j:%d\n", i, j)
+			if i == 1 && j == 1 {
+				fmt.Println("找到目标，跳出循环！")
+				break OutterLoop //使用 break + 标签，直接跳出到 OuterLoop那一层
+			}
+		}
+	}
+	fmt.Printf("进程结束")
+}
