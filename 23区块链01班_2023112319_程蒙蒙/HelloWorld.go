258a56199a94fc66c0b2371fc7572bf9363f9784
yydsjs <2508674706@qq.com>
2025-10-17 14:57:28 +0800

    Update HelloWorld.go

--- 23区块链01班_2023112319_程蒙蒙/HelloWorld.go
+++ 23区块链01班_2023112319_程蒙蒙/HelloWorld.go
@@ -8,3 +8,72 @@ func main() {
 	c := 20 //第一次赋值才能用
 	fmt.Println("hello world:", a, b, c)
 }
+
+//package main
+
+//import "fmt"
+
+// func main() {
+// 	var a int = 10
+// 	var b = 10
+// 	c := 20 //第一次赋值才能用
+// 	fmt.Println("hello world:", a, b, c)
+// }
+
+//package main
+//import "fmt"
+// func main() {
+// 	a := 3
+// 	switch a {
+// 	case 1:
+// 		fmt.Println("a=1")
+
+// 	case 2:
+// 		fmt.Println("a=2")
+// 	default:
+// 		fmt.Println("default")
+// 	}
+// }
+
+//package main
+//import "fmt"
+// func main() {
+//     fmt.Println("Hello, World!")
+//     fmt.Println("欢迎 程蒙蒙 学习Go语言!")
+//     fmt.Println("这是你的第一个Go程序")
+
+//     // 基本变量声明
+//     var name string = "程蒙蒙"
+//     var age int = 20
+
+//     fmt.Printf("学生姓名: %s\n", name)
+//     fmt.Printf("年龄: %d\n", age)
+
+//     // 简单的函数调用
+//     greet(name)
+// }
+
+// // 问候函数
+// func greet(name string) {
+//     fmt.Printf("你好, %s! 欢迎来到Go语言的世界!\n", name)
+// }
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	for i := 0; i < 10; i++ {
+// 		fmt.Println(i)
+// 	}
+// }
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	for {
+// 		fmt.Println("hello world")
+// 	}
+// }
