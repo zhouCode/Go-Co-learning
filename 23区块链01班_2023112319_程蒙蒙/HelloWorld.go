da08a5a9bf7c69bfa489f83230dc752a59706969
yydsjs <2508674706@qq.com>
2025-11-21 14:50:41 +0800

    Update HelloWorld.go

--- 23区块链01班_2023112319_程蒙蒙/HelloWorld.go
+++ 23区块链01班_2023112319_程蒙蒙/HelloWorld.go
@@ -319,3 +319,26 @@
 // }
 
 
+//|-------------------第8章---------------------|
+ package main
+
+ import "fmt" 
+
+
+ func main() {
+
+	res , err3 := checkAge(-12)
+	if err3 != nil {
+		fmt.Println(err3.Error())
+	} else {
+		fmt.Println(res)
+	}
+}
+func checkAge(age int) (string ,error) {
+	if age < 0 {
+		err := fmt.Errorf("您的年龄输入是： %d , 该数值为负数，有错误！",age)
+		return "",err
+	}else {
+		return fmt.Sprintf("您的年龄输入是： %d ",age),nil
+	}
+}
